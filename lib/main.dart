a4d3ac9b7bb7c5310099b0a684fd4b2fa0b9e041
CyberWake <ritikfbd@gmail.com>
2020-02-21 15:30:36 +0530

    Made the UI for tracking page with static values and added a welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app/tracking.dart';
 import 'exercise.dart';
 import 'tracking.dart';
 import 'me.dart';
+import 'welcome.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -11,26 +12,33 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title:Text("test app"),
-          backgroundColor: Colors.lightBlue,
+        title: 'Fitness App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Color(0XFF6D3FFF),
+          accentColor: Color(0XFF233C63),
+          fontFamily: 'Poppins',
         ),
-        body:Exercise(),
-        bottomNavigationBar: CurvedNavigationBar(
-            backgroundColor: Colors.transparent,
-            index: 0,
-            height: 55,
-            buttonBackgroundColor: Colors.white,
-            animationDuration: Duration(milliseconds:225),
-            items: <Widget>[
-              Icon(MdiIcons.yoga, size: 30),
-              Icon(MdiIcons.trendingUp, size: 30),
-              Icon(MdiIcons.account, size: 30),
-            ]
-        ),
-      )
+        home:Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title:Text("test app"),
+            backgroundColor: Colors.lightBlue,
+          ),
+          body:Welcome(),
+          bottomNavigationBar: CurvedNavigationBar(
+              backgroundColor: Colors.transparent,
+              index: 0,
+              height: 55,
+              buttonBackgroundColor: Colors.grey,
+              animationDuration: Duration(milliseconds:225),
+              items: <Widget>[
+                Icon(MdiIcons.yoga, size: 30),
+                Icon(MdiIcons.trendingUp, size: 30),
+                Icon(MdiIcons.account, size: 30),
+              ]
+          ),
+        )
     );
   }
 }
